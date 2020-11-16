500143879988e3d9f8c2a43099371e6c6246cf61
Kamil Matula <43967269+MKamil99@users.noreply.github.com>
2020-11-17 00:14:15 +0100

    Fixed movement on the slopes

--- SheepsSword/Assets/Scripts/Player/PlayerController.cs
+++ SheepsSword/Assets/Scripts/Player/PlayerController.cs
@@ -1,6 +1,4 @@
-﻿// TO DO: FIX MOVEMENT ON SLOPES
-
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.UI;
 
 public class PlayerController : MonoBehaviour
@@ -27,20 +25,21 @@ public class PlayerController : MonoBehaviour
     private GameObject hitPointRight;   // for attacking
     private GameObject hitPointLeft;
 
-    // Parameters:
+    // Other parameters:
+    private readonly float slopeCheckerRadius = 0.6f;
     private readonly float colliderReductor = 0.8f;
     private readonly float animationLength = 0.25f;
-    private int  attackViewNumber = -1;
-    private bool canSomerSault = false;
-    private bool isAttacking = false;
-    private bool isSomerSaulting = false;
-    private bool isGrounded = false;  // contact with the ground 
-    private bool isCeilinged = false; // contact with the ceiling
-    private bool isCrouched = false;
-    private int  isWalled = 0;        // contact with the wall
-    private bool isDead = false;
-    private bool isHurting = false;
-    private bool isSliding = false;
+    private int attackViewNumber = -1;
+    private bool canSomerSault;
+    private bool isAttacking;
+    private bool isSomerSaulting;
+    private bool isGrounded;  // contact with the ground 
+    private bool isCeilinged; // contact with the ceiling
+    private bool isCrouched;
+    private int isWalled;     // contact with the wall
+    private bool isDead;
+    private bool isHurting;
+    private bool isSliding;
 
     // UI:
     private Text hpText;
@@ -69,11 +68,12 @@ public class PlayerController : MonoBehaviour
         gameoverText.gameObject.SetActive(false);
     }
 
-    void Update()
+    private void Update()
     {
         CheckTheGround();
         CheckTheWall();
         CheckTheCeiling();
+        //CheckTheSlope();
 
         Jump();
         Crouch();
@@ -91,7 +91,6 @@ public class PlayerController : MonoBehaviour
         isGrounded = (collider != null);
     }
 
-    // The Player Transform consists of two wall checkers so we know if the wall is on the right or on the left.
     private void CheckTheWall()
     {
         // Checking if player is on the wall:
@@ -113,36 +112,46 @@ public class PlayerController : MonoBehaviour
 
 
 
-    private void Move() // Run and Climb
+    private void Move()
     {
+        //Check if you can go:
+        Vector2 slopeCheckerPosition = transform.position - new Vector3(0.0f, capscol.size.y / 2, 0.0f);
+        RaycastHit2D slopeHitRight = Physics2D.Raycast(slopeCheckerPosition,
+            transform.right, slopeCheckerRadius, groundLayer);
+        RaycastHit2D slopeHitLeft = Physics2D.Raycast(slopeCheckerPosition,
+            -transform.right, slopeCheckerRadius, groundLayer);
+
+
         // Run (horizontal movement) + avoid auto-sliding down on slopes:
         float xMove = Input.GetAxisRaw("Horizontal");
         if (xMove != 0)
         {
+
             rigbody.constraints = RigidbodyConstraints2D.FreezeRotation;
             rigbody.velocity = new Vector2(xMove * model.Speed, rigbody.velocity.y);
 
-            if (isGrounded) // MOVEMENT ON SLOPES!
+            // Running on the slopes:
+            RaycastHit2D hit = Physics2D.Raycast(groundChecker.position, Vector2.down, 
+                groundCheckerRadius, groundLayer);
+            if (isGrounded && Input.GetAxisRaw("Vertical") == 0 &&
+                hit.collider != null && Mathf.Abs(hit.normal.x) > 0.1f)
             {
-                //RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.down, 1f, groundLayer);
-                //if (hit.collider != null && Mathf.Abs(hit.normal.x) > 0.1f && hit.normal.x * xMove > 0)
-                //    rigbody.velocity = new Vector2(rigbody.velocity.x, -model.JumpForce);
+                if (hit.normal.x * xMove > 0) // running down
+                    rigbody.velocity = new Vector2(rigbody.velocity.x, -model.JumpForce);
+                else if (!slopeHitLeft && !slopeHitRight) // running up (at the end of the slope)
+                    rigbody.velocity = new Vector2(rigbody.velocity.x, 1.0f);
             }
         }
+        // Stopping on the slope => freezing whole movement:
+        else if (Input.GetAxisRaw("Vertical") == 0 && isGrounded && isWalled == 0 &&
+            ((slopeHitLeft && !view.LookRight) || (slopeHitRight && view.LookRight)))
+             rigbody.constraints = RigidbodyConstraints2D.FreezePositionX
+                                 | RigidbodyConstraints2D.FreezePositionY
+                                 | RigidbodyConstraints2D.FreezeRotation;
+        // Not moving => freezing on the X:
         else rigbody.constraints = RigidbodyConstraints2D.FreezePositionX
                                  | RigidbodyConstraints2D.FreezeRotation;
 
-        // Flip:
-        if (view.LookRight == true && xMove < 0)
-        {
-            playerGraphics.GetComponent<SpriteRenderer>().flipX = true;
-            view.LookRight = false;
-        }
-        if (view.LookRight == false && xMove > 0)
-        {
-            playerGraphics.GetComponent<SpriteRenderer>().flipX = false;
-            view.LookRight = true;
-        }
 
         // Climb (vertical movement) + avoid auto-sliding down on walls:
         if (isWalled != 0 && !isCeilinged)
@@ -190,8 +199,9 @@ public class PlayerController : MonoBehaviour
 
     private void SomerSaultCompleted() { isSomerSaulting = false; }
 
-    private void FastFall() // after clicking DownArrow (or S), player goes down faster
+    private void FastFall()
     {
+        // after clicking DownArrow (or S), player goes down faster
         if (!isGrounded) 
             if (Input.GetKeyDown(KeyCode.DownArrow) || Input.GetKeyDown(KeyCode.S))
                 rigbody.velocity = new Vector2(0, -model.JumpForce / 1.5f);
@@ -232,8 +242,7 @@ public class PlayerController : MonoBehaviour
 
     private void Slide()
     {
-        float xMove = Input.GetAxisRaw("Horizontal");
-        if (xMove != 0 && !isCrouched && isWalled == 0 && !isAttacking && !isHurting)
+        if (Input.GetAxisRaw("Horizontal") != 0 && !isCrouched && isWalled == 0 && !isAttacking && !isHurting)
         {
             isSliding = true;
             AttackStart(); // hit just at the beginning of the animation
@@ -276,6 +285,19 @@ public class PlayerController : MonoBehaviour
 
     private void Animate()
     {
+        // Flip:
+        if (view.LookRight == true && Input.GetAxisRaw("Horizontal") < 0)
+        {
+            playerGraphics.GetComponent<SpriteRenderer>().flipX = true;
+            view.LookRight = false;
+        }
+        if (view.LookRight == false && Input.GetAxisRaw("Horizontal") > 0)
+        {
+            playerGraphics.GetComponent<SpriteRenderer>().flipX = false;
+            view.LookRight = true;
+        }
+
+        // Change animation:
         if (isHurting) view.Hurt();
         else if (isDead) view.Die();
         else if (isSliding) view.Slide();
