0e2eb03bd73b2d7441b32a95c329c6bfae938180
Dariusz <dariusz.momot98@gmail.com>
2021-01-23 22:15:46 +0100

    Add Worm Enemy

---
+++ SheepsSword/Assets/Scripts/Worm/Worm_Model.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Worm_Model : MonoBehaviour
+{
+    [SerializeField]
+    private int _hp;
+    public int HP
+    {
+        get { return _hp; }
+        set { _hp = value; }
+    }
+
+    [SerializeField]
+    private float _speed;
+    public float Speed
+    {
+        get { return _speed; }
+        set { _speed = value; }
+    }
+}
