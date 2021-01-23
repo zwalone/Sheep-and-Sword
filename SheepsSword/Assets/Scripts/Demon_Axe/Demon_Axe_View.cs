a16397987c6e9e951e62404e4a3feab1654745e5
Dariusz <dariusz.momot98@gmail.com>
2021-01-23 22:50:38 +0100

    Add Demon Axe

---
+++ SheepsSword/Assets/Scripts/Demon_Axe/Demon_Axe_View.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Demon_Axe_View : MonoBehaviour
+{
+    private enum Anim
+    {
+        Walk,
+        Attack1,
+        Dead,
+        Hit,
+    }
+
+    private Anim _currentState;
+
+    private Animator _animator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _animator = this.GetComponent<Animator>();
+        _currentState = Anim.Walk;
+    }
+
+    private void ChangeAnimState(Anim state)
+    {
+        //Stop the same animation playing
+        if (_currentState == state) return;
+
+        _currentState = state;
+
+        _animator.Play(_currentState.ToString());
+    }
+
+    public void Walk()
+    {
+        ChangeAnimState(Anim.Walk);
+    }
+
+    public void Attack()
+    {
+        ChangeAnimState(Anim.Attack1);
+    }
+
+    public void TakeDamage()
+    {
+        ChangeAnimState(Anim.Hit);
+    }
+
+
+    public void Die()
+    {
+        ChangeAnimState(Anim.Dead);
+    }
+}
