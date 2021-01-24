ae6362efecc51532200c5a786d2bef135cf0bc94
Dariusz <dariusz.momot98@gmail.com>
2021-01-24 22:18:20 +0100

    add secoud boss

---
+++ SheepsSword/Assets/Scripts/Dark_Boss/Dark_Boss_Controller.cs
@@ -0,0 +1,262 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dark_Boss_Controller : MonoBehaviour, IEntityController
+{
+    public Transform rayCast;
+    public LayerMask rayCastMask;
+    public float rayCastLength;
+    public float attackDistance;
+
+    private GameObject target;
+    private bool _inRange;
+
+    private Dark_Boss_Model _model;
+    private Dark_Boss_View _view;
+
+    private Rigidbody2D _rd2D;
+
+    [SerializeField]
+    private GameObject[] hitbox;
+
+    [SerializeField]
+    private CircleCollider2D _isGroundBottom;
+
+    [SerializeField]
+    private CircleCollider2D _isGroundOpposite;
+
+
+    // Parameters:
+    [SerializeField]
+    private bool _changeDirection;
+
+    private bool _isAttacking = false;
+    private bool _canUseAttack = true;
+    private bool _canDash = true;
+    private bool _isDash = false;
+
+    public bool IsHurting { get; private set; }
+    public bool IsDead { get; private set; }
+    private int AttackNumber;
+
+
+
+
+    private void Awake()
+    {
+        _view = this.GetComponent<Dark_Boss_View>();
+        _model = this.GetComponent<Dark_Boss_Model>();
+        _rd2D = this.GetComponent<Rigidbody2D>();
+    }
+
+    void Start()
+    {
+        _changeDirection = true;
+    }
+
+    private void FixedUpdate()
+    {
+        //Move Enemy and check direction
+        _rd2D.MovePosition(_rd2D.position + new Vector2(_model.Speed, 0) * Time.fixedDeltaTime);
+        ChangeMoveDirection();
+    }
+
+    private void Update()
+    {
+        Animate();
+        if (_inRange) CheckAttack();
+    }
+
+
+
+    //Attack
+    private void CheckAttack()
+    {
+        if (!_isAttacking && _canUseAttack)
+        {
+            AttackNumber = Random.Range(0, 3);
+            AttackStart();
+
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collider)
+    {
+
+        if (collider.gameObject.CompareTag("Player")
+            && !collider.gameObject.GetComponentInParent<IEntityController>().IsDead)
+        {
+            _inRange = true;
+            target = collider.gameObject;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            _inRange = false;
+            target = null;
+        }
+    }
+
+    private void AttackStart()
+    {
+
+        _isAttacking = true;
+        _canUseAttack = false;
+
+        Invoke(nameof(CanUseAttack), 4f);
+
+        if (AttackNumber != 2)
+        {
+            Invoke(nameof(AttackStop), 1.1f);
+
+            foreach (var h in hitbox)
+            {
+                h.GetComponent<BoxCollider2D>().enabled = true;
+                //TOChange
+                _model.Speed *= 3;
+            }
+        }
+        else
+        {
+            Heal();
+        }
+        
+    }
+
+    private void AttackStop()
+    {
+        _model.Speed /= 3;
+        _isAttacking = false;
+        foreach (var h in hitbox)
+        {
+            h.GetComponent<BoxCollider2D>().enabled = false;
+        }
+    }
+
+    private void Heal()
+    {
+        var prevSpeed = _model.Speed;
+        //_model.Speed = 0;
+        _model.HP += 10;
+        Invoke(nameof(IsAttack), 1.1f);
+
+        Debug.Log("HEal");
+    }
+
+    //Check and Change direction
+    private void ChangeMoveDirection(bool behind = false)
+    {
+        if (IsDead || _isAttacking) return;
+
+        if (behind)
+        {
+            _changeDirection = false;
+            StartCoroutine(ChangeDirectionCorutine());
+        }
+
+        if (!_isGroundBottom.IsTouchingLayers(LayerMask.GetMask("Ground")) && _changeDirection)
+        {
+            _changeDirection = false;
+            StartCoroutine(ChangeDirectionCorutine());
+        }
+        else if (_isGroundOpposite.IsTouchingLayers(LayerMask.GetMask("Ground")) && _changeDirection)
+        {
+            _changeDirection = false;
+            StartCoroutine(ChangeDirectionCorutine());
+        }
+    }
+
+    //Coroutine for Movement
+    IEnumerator ChangeDirectionCorutine()
+    {
+        _model.Speed = -_model.Speed;
+        this.transform.localRotation *= Quaternion.Euler(0, 180, 0);
+        yield return new WaitForSeconds(0.7f);
+        _changeDirection = true;
+    }
+
+    public void TakeDamage(int dmg)
+    {
+        //check distance
+        var p = GameObject.FindGameObjectWithTag("Player").transform;
+        Vector3 toTarget = (p.position - transform.position).normalized;
+        if (Vector3.Dot(toTarget, transform.forward) < 0)
+        {
+            ChangeMoveDirection(true);
+        }
+
+
+        if (!_canDash)
+        {
+            _model.HP -= dmg;
+            if (_model.HP <= 0)
+            {
+                _model.HP = 0;
+                _model.Speed = 0;
+
+                IsDead = true;
+                Invoke(nameof(DestroyMe), 1.0f);
+            }
+            else
+            {
+                IsHurting = true;
+                Invoke(nameof(StopHurting), 0.2f);
+            }
+        }
+        else
+        {
+            Dash();
+        }
+
+    }
+
+    private void Dash()
+    {
+        _canDash = false;
+        _isDash = true;
+
+        gameObject.layer = 30;
+        gameObject.GetComponentInChildren<HitBoxController>().damage = 0;
+        _model.Speed *= 3;
+
+        Invoke(nameof(StopDashing), 0.3f);
+        //Delay Dash
+        Invoke(nameof(CanDash), 5f);
+    }
+    private void StopDashing()
+    {
+        _isDash = false;
+        gameObject.layer = 0;
+        gameObject.GetComponentInChildren<HitBoxController>().damage = 5;
+        _model.Speed /= 3;
+    }
+
+
+
+    private void DestroyMe() { Destroy(gameObject); }
+    private void StopHurting() { IsHurting = false; }
+    private void CanUseAttack() { _canUseAttack = true; }
+    private void CanDash() { _canDash = true; }
+    private void IsAttack() {
+        _isAttacking = false; 
+    }
+
+
+    private void Animate()
+    {
+        if (IsDead) _view.Die();
+        else if (IsHurting) _view.TakeDamage();
+        else if (_isDash) _view.Dash();
+        else if (_isAttacking && !_canUseAttack)
+        {
+            if (AttackNumber == 0) _view.Attack1();
+            else if (AttackNumber == 1) _view.Attack2();
+            else if (AttackNumber == 2) _view.Heal();
+        }
+        else _view.Walk();
+    }
+}
---
+++ SheepsSword/Assets/Scripts/Dark_Boss/Dark_Boss_View.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dark_Boss_View : MonoBehaviour
+{
+    private enum Anim
+    {
+        Dark_Attack1,
+        Dark_Attack2,
+        Dark_Idle, //hit
+        Dark_Magic, //heal
+        Dark_Run, //walk
+        Dark_Dash,
+        Die
+    }
+
+    private Anim _currentState;
+
+    private Animator _animator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _animator = this.GetComponent<Animator>();
+        _currentState = Anim.Dark_Run;
+    }
+
+    private void ChangeAnimState(Anim state)
+    {
+        //Stop the same animation playing
+        if (_currentState == state) return;
+
+        _currentState = state;
+
+        _animator.Play(_currentState.ToString());
+    }
+
+    public void Walk()
+    {
+        ChangeAnimState(Anim.Dark_Run);
+    }
+
+    public void Dash()
+    {
+        ChangeAnimState(Anim.Dark_Dash);
+    }
+
+    public void Attack2()
+    {
+        ChangeAnimState(Anim.Dark_Attack2);
+    }
+
+    public void Attack1()
+    {
+        ChangeAnimState(Anim.Dark_Attack1);
+    }
+
+    public void TakeDamage()
+    {
+        ChangeAnimState(Anim.Dark_Idle);
+    }
+
+    public void Die()
+    {
+        ChangeAnimState(Anim.Die);
+    }
+
+    public void Heal()
+    {
+        ChangeAnimState(Anim.Dark_Magic);
+    }
+}
