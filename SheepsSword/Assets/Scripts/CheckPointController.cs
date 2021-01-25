89318421e505b7f57e9828796004f2d1175ce2b0
Kamil Matula <43967269+MKamil99@users.noreply.github.com>
2021-01-25 13:17:12 +0100

    Checkpoint animation and sound

--- SheepsSword/Assets/Scripts/CheckPointController.cs
+++ SheepsSword/Assets/Scripts/CheckPointController.cs
@@ -4,19 +4,42 @@ public class CheckPointController : MonoBehaviour
 {
     private GameController gm;
     public bool isFirst;
+    private bool hasBeenReached = false;
 
     private void Awake()
     {
         gm = GameObject.FindGameObjectWithTag("GameController").GetComponent<GameController>();
-        if (isFirst && gm.LastCheckpointPosition.x == 0 && gm.LastCheckpointPosition.y == 0) 
+        if (isFirst && gm.WaitingForFirstPosition)
+        {
+            gm.WaitingForFirstPosition = false;
             gm.LastCheckpointPosition = transform.position;
+            foreach (Transform child in transform)
+            {
+                if (child.CompareTag("Checkpoint_Unreached"))
+                    child.gameObject.SetActive(false);
+                else if (child.CompareTag("Checkpoint_Reached"))
+                    child.gameObject.SetActive(true);
+            }
+            hasBeenReached = true;
+        }
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
+        if (hasBeenReached) return;
+
         if (collision.CompareTag("Player"))
         {
             gm.LastCheckpointPosition = transform.position;
+            foreach(Transform child in transform)
+            {
+                if (child.CompareTag("Checkpoint_Unreached"))
+                    child.gameObject.SetActive(false);
+                else if (child.CompareTag("Checkpoint_Reached"))
+                    child.gameObject.SetActive(true);
+            }
+            hasBeenReached = true;
+            gameObject.GetComponent<AudioSource>().Play();
         }
     }
 }
--- SheepsSword/Assets/Scripts/GameController.cs
+++ SheepsSword/Assets/Scripts/GameController.cs
@@ -4,6 +4,7 @@ using UnityEngine.SceneManagement;
 public class GameController : MonoBehaviour
 {
     private static GameController instance;
+    public bool WaitingForFirstPosition = true;
     public Vector2 LastCheckpointPosition; 
 
     private void Awake() 
