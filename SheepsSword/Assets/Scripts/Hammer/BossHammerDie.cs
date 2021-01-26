58649cad38b0dfc1a14a3d1b7158333317c3e3d8
Dariusz <dariusz.momot98@gmail.com>
2021-01-26 21:30:19 +0100

    Add some prefabs to map and change FG2 to little smaller

---
+++ SheepsSword/Assets/Scripts/Hammer/BossHammerDie.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossHammerDie : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject Wall;
+
+    private void FixedUpdate()
+    {
+        if (gameObject.GetComponent<Hammer_Model>().HP <= 0)
+        {
+            Wall.SetActive(false);
+        }
+    }
+}
