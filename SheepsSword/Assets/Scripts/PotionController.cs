84764a249888ae8cf00346c0d9163e7b0dadce59
Kamil Matula <43967269+MKamil99@users.noreply.github.com>
2021-01-27 11:31:53 +0100

    Potion sound and player health bar update

--- SheepsSword/Assets/Scripts/PotionController.cs
+++ SheepsSword/Assets/Scripts/PotionController.cs
@@ -1,24 +1,30 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using UnityEngine.UI;
 
-public class Potions : MonoBehaviour
+public class PotionController : MonoBehaviour
 {
     [SerializeField]
     private int heal = 50;
+
+    private Image playerHealthBar;
+
+    private void Awake()
+    {
+        playerHealthBar = GameObject.Find("PlayerHealthBar_Fill").GetComponent<Image>();
+    }
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("Player"))
         {
             var model = collision.gameObject.GetComponent<PlayerModel>();
-            Debug.Log("heal");
             if(model.HP != model.MaxHP)
             {
+                collision.gameObject.GetComponent<SoundController>().PlaySound(6);
                 model.HP += heal;
-                Destroy(this.gameObject);
-
+                playerHealthBar.fillAmount = model.HP / model.MaxHP;
+                Destroy(gameObject);
             }
-
         }
     }
 }
