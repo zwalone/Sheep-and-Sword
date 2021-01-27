b27c3a030e8c28a1ff02b713082bd81a1b369935
Dariusz <dariusz.momot98@gmail.com>
2021-01-27 15:09:37 +0100

    Changed GM2 boss fight map

--- SheepsSword/Assets/Scripts/Hammer/BossHammerDie.cs
+++ SheepsSword/Assets/Scripts/Hammer/BossHammerDie.cs
@@ -7,11 +7,18 @@ public class BossHammerDie : MonoBehaviour
     [SerializeField]
     private GameObject Wall;
 
+    [SerializeField]
+    private GameObject[] Hole;
+
     private void FixedUpdate()
     {
         if (gameObject.GetComponent<Hammer_Model>().HP <= 0)
         {
             Wall.SetActive(false);
+            foreach (var h in Hole)
+            {
+                h.SetActive(false);
+            }
         }
     }
 }
