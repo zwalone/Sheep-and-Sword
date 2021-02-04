3cadb1d3f6a9fe0d0481f15fe586de5871f07a24
Dariusz <dariusz.momot98@gmail.com>
2021-02-04 17:09:37 +0100

    Add slider with new algorithm

--- SheepsSword/Assets/Scripts/Menu/VolumeMenu.cs
+++ SheepsSword/Assets/Scripts/Menu/VolumeMenu.cs
@@ -2,12 +2,41 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Audio;
+using UnityEngine.UI;
 
 public class VolumeMenu : MonoBehaviour
 {
+
     public AudioMixer audioMixer;
-    public void SetValume(float volume)
+    public Slider music;
+    public Slider dialog;
+    public Slider effect;
+    private void Awake()
     {
-        audioMixer.SetFloat("volume", volume);
+        float val;
+
+        if (audioMixer.GetFloat("Effect", out val))
+            effect.value = Mathf.Pow(10, val/20);
+
+        if (audioMixer.GetFloat("Dialog", out val))
+            dialog.value = Mathf.Pow(10, val/20);
+
+        if (audioMixer.GetFloat("Music", out val))
+            music.value = Mathf.Pow(10, val/20 );
+    }
+
+    public void SetMusic(float volume)
+    {
+        audioMixer.SetFloat("Music", Mathf.Log10(volume)*20);
+    }
+
+    public void SetDialog(float volume)
+    {
+        audioMixer.SetFloat("Dialog", Mathf.Log10(volume)*20);
+    }
+
+    public void SetEffect(float volume)
+    {
+        audioMixer.SetFloat("Effect", Mathf.Log10(volume)*20);
     }
 }
