f95742909bf85d4c1496abd0296c4e3351189b31
Dariusz <dariusz.momot98@gmail.com>
2021-02-06 16:25:11 +0100

    Add cluds in outro

---
+++ Sheep-and-Sword/Assets/Scripts/OutroClouds.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OutroClouds : MonoBehaviour
+{
+    public float SpeedClouds = 2f;
+
+    void Update()
+    {
+        this.transform.Translate(Vector3.right * Time.deltaTime * SpeedClouds);
+    }
+}
